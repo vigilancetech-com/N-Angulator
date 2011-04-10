f383423cf09ff5920b9f22fcafa85d08c6c974d1
KEVIN HADDOCK <kevinhaddock@yahoo.com>
2011-04-10 13:32:59 -0700

    fixed problems launching file with space, added sxemacs.desktop and sxe script to archive

--- na.el
+++ na.el
@@ -439,7 +439,7 @@ value if this widget did not override the parent"
 			'leaf-edit)
 		   (let ((name (widget-get widget :tag)))
 
-		     (start-process ("view" *scratch* "xdg-open" name ))
+		     (setq foo (start-process "view" "*scratch*" "xdg-open" (concat "\"" name "\"" ))))
 ;		     (eshell-command (concat "xdg-open " "\"" name "\" &") "*scratch*"))
 			 ;There appears to be junk left in ~/tmp -- needs to be del'd
 		     (funcall (na-default 'leaf :value-set) widget value))))
